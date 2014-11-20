bcefc92d4c25ccbd75a008f6aaa21720de94fe0f
blorente <blorente@ucm.es>
2014-11-20 17:42:15 +0100

    Filled the GameRules interface

--- Project 2/tp/pr2/logic/ComplicaRules.java
+++ Project 2/tp/pr2/logic/ComplicaRules.java
@@ -1,5 +1,50 @@
 package tp.pr2.logic;
 
-public class ComplicaRules {
+public class ComplicaRules implements GameRules {
+
+	/* (non-Javadoc)
+	 * @see tp.pr2.logic.GameRules#initializeBoard()
+	 */
+	@Override
+	public Board initializeBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see tp.pr2.logic.GameRules#initialPlayer()
+	 */
+	@Override
+	public Counter initialPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see tp.pr2.logic.GameRules#isWinner(tp.pr2.logic.Move, tp.pr2.logic.Board)
+	 */
+	@Override
+	public Counter isWinner(Move lastMove, Board b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see tp.pr2.logic.GameRules#draw(tp.pr2.logic.Counter, tp.pr2.logic.Board)
+	 */
+	@Override
+	public boolean draw(Counter lastPlaced, Board b) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see tp.pr2.logic.GameRules#nextTurn(tp.pr2.logic.Counter, tp.pr2.logic.Board)
+	 */
+	@Override
+	public Counter nextTurn(Counter lastPlaced, Board b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- Project 2/tp/pr2/logic/GameRules.java
+++ Project 2/tp/pr2/logic/GameRules.java
@@ -1,5 +1,44 @@
+/**
+ * 
+ */
 package tp.pr2.logic;
 
-public class GameRules {
-
+public interface GameRules {
+	
+	/** 
+	 * Constructs the board that has to be used for the game, 
+	 * according to the rules of the game.
+	 * @return The board and it's state at the beginning of the game
+	 */
+	public Board initializeBoard();
+	
+	/**
+	 * Returns the counter of the player starting the game
+	 * @return The counter of the player starting the game
+	 */
+	public Counter initialPlayer();
+	
+	/**
+	 * Checks whether there's a winner in the game.
+	 * @param lastMove Last move made
+	 * @param b Current board state
+	 * @return The counter of the winner (if there is one)
+	 */
+	public Counter isWinner(Move lastMove, Board b);
+	
+	/**
+	 * Returns whether the game is in a draw
+	 * @param lastPlaced Last player to play their counter
+	 * @param b Current board state
+	 * @return true if the game is ended without a winner
+	 */
+	public boolean draw(Counter lastPlaced, Board b);
+	
+	/**
+	 * Returns the player that has to play the next counter.
+	 * @param lastPlaced Last player to play their counter
+	 * @param b Current board state
+	 * @return next player to place a counter
+	 */
+	public Counter nextTurn(Counter lastPlaced, Board b);
 }
